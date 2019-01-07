3375852de70045267039bd56b2cd8d91b1ddbcd3
Scott Leaney <Scott.Leaney@arm.com>
2019-01-07 15:09:40 +0000

    Added a new request mapping that accepts a GET or POST. On page load you will be redirected to the hello_world test.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world!"})
--- helloworld/urls.py
+++ helloworld/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from api import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/hello_world/')),
     path('admin/', admin.site.urls),
+    path('hello_world/', views.hello_world)
 ]
